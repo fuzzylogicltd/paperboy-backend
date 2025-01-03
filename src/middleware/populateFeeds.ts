d024b9b71507681891d5ec8e165967f1d70d28bd
Lazar <lazar@fuzzylogic.ltd>
2025-01-04 00:28:50 +0100

    Add feed fetch, parse and insert

--- src/middleware/populateFeeds.ts
+++ src/middleware/populateFeeds.ts
@@ -1,9 +1,13 @@
 import prisma from "../db";
+import { XMLParser } from "fast-xml-parser";
+import got from "got";
 
 export const populateOneFeed = async (req, res, next) => {
+  const feedId = Number(req.params.id);
   const feed = await prisma.feed.findFirst({
     where: {
-      id: Number(req.params.id),
+      id: feedId,
+      // TODO: add rate limit based on last fetched date
     },
     select: {
       url: true,
@@ -19,9 +23,12 @@ export const populateOneFeed = async (req, res, next) => {
     },
   });
 
-  const newArticles = await fetchArticlesFromRSS(feed.url);
+  const newArticles = await fetchArticlesFromRSS(feed.url, feedId);
 
-  // insert new articles into articles
+  await prisma.article.createMany({
+    data: newArticles,
+    skipDuplicates: true,
+  });
 
   next();
 };
@@ -34,6 +41,7 @@ export const populateManyFeeds = async (req, res, next) => {
           userId: req.user.id,
         },
       },
+      // TODO: add rate limit based on last fetched date
     },
     select: {
       id: true,
@@ -51,14 +59,50 @@ export const populateManyFeeds = async (req, res, next) => {
   });
 
   feeds.forEach(async (feed) => {
-    const newArticles = await fetchArticlesFromRSS(feed.url);
+    const newArticles = await fetchArticlesFromRSS(feed.url, feed.id);
 
-    // insert
+    await prisma.article.createMany({
+      data: newArticles,
+      skipDuplicates: true,
+    });
   });
 
   next();
 };
 
-async function fetchArticlesFromRSS(url) {
-  console.log("fetching " + url);
+async function fetchArticlesFromRSS(url, feedId) {
+  const buffer = await got(url).buffer();
+
+  const parser = new XMLParser({
+    ignoreAttributes: false,
+    attributeNamePrefix: "@_",
+  });
+  const feed = await parser.parse(buffer);
+
+  let items = [];
+
+  // TODO: refactor below this line ------
+
+  if (feed.feed) {
+    // atom
+    items = feed.feed.entry;
+  } else {
+    // rss
+    items = feed.rss.channel.item;
+  }
+
+  const formattedItems = items.map((item) => {
+    const formattedDate = new Date(item.pubDate ?? item.updated);
+
+    return {
+      feedId: feedId,
+      title: item.title,
+      url: item.link["@_href"] ?? item.link,
+      description: item.description ?? "",
+      datePublished: formattedDate,
+      body: item["content:encoded"] ?? item.summary["#text"] ?? "",
+    };
+  });
+
+  return formattedItems;
 }
