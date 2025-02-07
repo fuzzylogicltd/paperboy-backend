2850320f569d0d7a0944d3b03821ed52a74376fb
Lazar <lazar@fuzzylogic.ltd>
2025-02-07 17:03:05 +0100

    Switch Protect middleware to cookie validation

--- src/modules/auth.ts
+++ src/modules/auth.ts
@@ -20,15 +20,7 @@ export const createJWT = (user) => {
 };
 
 export const protect = (req, res, next) => {
-  const bearer = req.headers.authorization;
-
-  if (!bearer) {
-    res.status(401);
-    res.json({ message: "not authorized" });
-    return;
-  }
-
-  const [, token] = bearer.split(" ");
+  const token = req.cookies.token;
 
   if (!token) {
     res.status(401);
