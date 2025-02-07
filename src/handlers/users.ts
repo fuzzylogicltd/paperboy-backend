c51dd27e38d622c574f5977ea91573df363c6df0
Lazar <lazar@fuzzylogic.ltd>
2025-02-07 12:32:45 +0100

    Switch auth to cookie

--- src/handlers/users.ts
+++ src/handlers/users.ts
@@ -29,5 +29,12 @@ export const signIn = async (req, res) => {
   }
 
   const token = createJWT(user);
-  res.json({ token });
+
+  res.cookie("token", token, {
+    httpOnly: true,
+    secure: process.env.NODE_ENV === "production",
+    sameSite: "Strict",
+  });
+
+  res.json("Authentication success");
 };
